211981eebe8789283ec6b70428e3e9b75007f870
Vladimir Sumilin <vladimirshumilin42@gmail.com>
2021-04-28 10:09:41 +0200

    FirstStep
    Hello guys!

---
+++ ProjectSchool/Models/Schedule.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectSchool.Models
+{
+    public class Schedule
+    {
+        public int ScheduleId { get; set; }
+        public DateTime Date { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public int CourseId { get; set; }
+
+    }
+}
---
+++ ProjectSchool/Models/Teacher.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectSchool.Models
+{
+    public class Teacher
+    {
+        public int TeacherId { get; set;}
+
+
+    }
+}
