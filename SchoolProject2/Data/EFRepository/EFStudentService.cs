0ce617060bfdbec27d65d48a50c62d5b8e71b4b6
Vladimir Sumilin <vladimirshumilin42@gmail.com>
2021-04-29 11:46:42 +0200

    FirstChange

---
+++ SchoolProject2/Data/EFRepository/EFStudentService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchoolProject2.Data.EFRepository
+{
+    public class EFStudentService
+    {
+        string ahah;
+    }
+}
