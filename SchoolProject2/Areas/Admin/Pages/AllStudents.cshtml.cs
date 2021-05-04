7156a322c2e03e5efe582375cc344a8d3ec21d1e
Vladimir Sumilin <vladimirshumilin42@gmail.com>
2021-05-04 19:20:37 +0200

    AdminPage

--- SchoolProject2/Areas/Admin/Pages/AllStudents.cshtml.cs
+++ SchoolProject2/Areas/Admin/Pages/AllStudents.cshtml.cs
@@ -2,12 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using SchoolProject2.Data;
 using SchoolProject2.Data.Repository;
 using SchoolProject2.Models;
+using SchoolProject2.Utility;
 
 namespace SchoolProject2.Areas.Admin.Pages
 {
--- SchoolProject2/Areas/Admin/Pages/Index.cshtml.cs
+++ SchoolProject2/Areas/Admin/Pages/Index.cshtml.cs
@@ -2,11 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SchoolProject2.Utility;
 
 namespace SchoolProject2.Areas.Admin.Pages
 {
+    [Authorize(Roles = SD.AdminUser)]
     public class IndexModel : PageModel
     {
         public void OnGet()
