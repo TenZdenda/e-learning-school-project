8ce8547d3ba5db9effcabb15157fbd80091382d5
Zdeněk Pašek <me@zdenekpasek.cz>
2021-05-24 13:42:33 +0200

    Auto stash before merge of "backend" and "origin/backend"

--- SchoolProjectTests/UnitTest1.cs
+++ SchoolProjectTests/UnitTest1.cs
@@ -1,3 +1,10 @@
+using Autofac.Extras.Moq;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using SchoolProject2.Data;
+using SchoolProject2.Data.EFRepository;
+using SchoolProject2.Data.Repository;
+using SchoolProject2.Models;
 using System;
 using Xunit;
 
@@ -6,9 +13,9 @@ namespace SchoolProjectTests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void LoadCourser_ValidCall()
         {
-
+            // TO-DO
         }
     }
 }
