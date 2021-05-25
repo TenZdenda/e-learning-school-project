f2ce0772e34b312e9f02b1bccd3d83ebb3c7e928
Zdeněk Pašek <me@zdenekpasek.cz>
2021-05-26 01:31:01 +0200

    Frontend bug fixes, Teacher page - AllCourses, AllSchedules

---
+++ SchoolProject2/Areas/Teacher/Pages/AllSchedules.cshtml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SchoolProject2.Data.Repository;
+using SchoolProject2.Models;
+using SchoolProject2.Utility;
+
+namespace SchoolProject2.Areas.Teacher.Pages
+{
+    [Authorize(Roles = SD.TeacherUser)]
+    public class AllSchedulesModel : PageModel
+    {
+        private readonly IAdminService _db;
+
+        public IEnumerable<Schedule> Schedules { get; set; }
+
+        [BindProperty]
+        public Schedule Schedule { get; set; }
+
+        public AllSchedulesModel(IAdminService db)
+        {
+            _db = db;
+        }
+
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            Schedules = await _db.GetAllSchedules();
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int id)
+        {
+            Schedule = await _db.GetScheduleByIdOrNullAsync(id);
+
+            var result = await _db.DeleteSchedule(id);
+
+            if (result)
+                TempData["SM"] = $"Schedule {(Schedule.Course.CourseName is null ? new Schedule() : Schedule.Course.CourseName)} has been successfully deleted";
+            else
+                TempData["FM"] = $"Schedule {(Schedule.Course.CourseName is null ? new Schedule() : Schedule.Course.CourseName)} has been failed to delete";
+
+            if (result)
+            {
+                Schedules = await _db.GetAllSchedules();
+                return Page();
+            }
+            return Page();
+        }
+    }
+}
