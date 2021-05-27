34d30a7378c6a01bef387cbfd1102906de24fa30
Zdeněk Pašek <me@zdenekpasek.cz>
2021-05-27 10:34:25 +0200

    Teacher/Student pages, bug fixes

---
+++ SchoolProject2/Areas/Teacher/Pages/Enrolments.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SchoolProject2.Data.Repository;
+using SchoolProject2.Models;
+
+namespace SchoolProject2.Areas.Teacher.Pages
+{
+    public class EnrolmentsModel : PageModel
+    {
+        private readonly ITeacherService _db;
+        public Course Courses { get; set; }
+        public EnrolmentsModel(ITeacherService db)
+        {
+            _db = db;
+        }
+        public async Task OnGetAsync(int id)
+        {
+            Courses = await _db.GetCourseAndStudentByIdAsync(id);
+        }
+
+        public async Task OnGetDeleteAsync(string userId, int courseId)
+        {
+            var result = await _db.RemoveUserFromCourseAsync(userId, courseId);
+
+            if (result)
+                TempData["SM"] = $"User has been successfully removed";
+            else
+                TempData["FM"] = $"User deleting failed";
+
+            Courses = await _db.GetCourseAndStudentByIdAsync(courseId);
+        }
+    }
+}
--- SchoolProject2/Startup.cs
+++ SchoolProject2/Startup.cs
@@ -53,6 +53,7 @@ namespace SchoolProject2
             services.AddRazorPages();
             services.AddScoped<IAdminService, EFAdminService>();
             services.AddScoped<IStudentService, EFStudentService>();
+            services.AddScoped<ITeacherService, EFTeacherService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
